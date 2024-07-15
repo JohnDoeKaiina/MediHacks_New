b9a7df141b3e5cd03f35f4816892de899e763909
annu12340 <annujolly17@gmail.com>
2024-07-15 10:25:04 +0530

    Updating the cron job for prescription

--- cron.py
+++ cron.py
@@ -1,10 +1,34 @@
 
-from datetime import datetime
+
+from datetime import datetime, time
 import sched
 import time
 import sqlite3
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
 
-# Function to check medicion schedule
+load_dotenv()
+
+def send_twilio_message(info):
+        print("Sending a remainder message from twilio")
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        client = Client(account_sid, auth_token)
+        twilio_phone_number = os.getenv("TWILIO_NUMBER")
+        phone_number =  os.getenv("TWILIO_DESTINATION_NUMBER")
+
+        # Set up Twilio client
+        client = Client(account_sid, auth_token)
+        msg =f'This is a reminder to take your medicine- {info[3]}. The quantity is {info[4]} and it must be taken at {info[6]} for {info[5]} days'
+        print(msg)
+        # Attempt to send the message
+        message = client.messages.create(
+            body=msg,
+            from_=twilio_phone_number,
+            to=phone_number
+        )
+
 def check_medicion_schedule():
     conn = sqlite3.connect('db.sqlite3')
     if conn:
@@ -13,15 +37,23 @@ def check_medicion_schedule():
             cursor.execute('SELECT * FROM user_prescrition')
             rows = cursor.fetchall()
             
-            current_time = datetime.now().time()  # Get current time
+            current_time = datetime.now().time() 
             
-            for row in rows:
-                medicion_time = row[1]  # Assuming the second column contains the medicion time
-                print("medicion_timemedicion_time",row)
-                if current_time.hour == medicion_time.hour and current_time.minute == medicion_time.minute:
-                    print("Time for medicion!")  # Perform your action here (e.g., send notification, execute task)
-                    break
-                
+            for row in rows:   
+                # Parse medicion_time_str into a datetime.time object
+                medicion_time_str = row[6]
+                medicine_status=row[7]
+                medicion_time = datetime.strptime(medicion_time_str, "%H:%M").time()
+
+                # Now you can compare current_time with medicion_time
+                if medicine_status=="Ongoing":
+                    
+                    if current_time.hour == medicion_time.hour and current_time.minute == medicion_time.minute:
+                        print(f"Current time {current_time} matches medicion_time {medicion_time} in row: {row}")
+                        send_twilio_message(row)
+
+                    else:
+                        print(f"Current time {current_time} does not match medicion_time {medicion_time} in row: {row}")
         except Exception as e:
             print("Error executing SQL query:", e)
         finally:
@@ -30,17 +62,17 @@ def check_medicion_schedule():
     else:
         print("Database connection not established.")
 
-# Scheduler instance
+
 scheduler = sched.scheduler(time.time, time.sleep)
 
-# Function to periodically check medicion schedule
+
 def periodic_check(sc):
     check_medicion_schedule()
     # Reschedule the check every 1 minute (adjust as needed)
     scheduler.enter(60, 1, periodic_check, (sc,))
 
-# Start the scheduler
+
 if __name__ == "__main__":
-    # Initial scheduling of the periodic check
+    # Initial scheduling 
     scheduler.enter(0, 1, periodic_check, (scheduler,))
     scheduler.run()
